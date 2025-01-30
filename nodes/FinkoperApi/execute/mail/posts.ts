23702eaf10373fbbb77618ef93013c13fe7382db
Stepan Grigorovich <gsdstr@gmail.com>
2025-01-30 23:22:19 +0300

    add seen

--- nodes/FinkoperApi/execute/mail/posts.ts
+++ nodes/FinkoperApi/execute/mail/posts.ts
@@ -2,18 +2,31 @@ import {IExecuteFunctions, IHttpRequestMethods} from "n8n-workflow";
 import {IDataObject, IHttpRequestOptions} from "n8n-workflow/dist/Interfaces";
 import {finkoperApiRequest} from "../finkoperRequest";
 
+type QueryParams = {
+	mailboxid: string;
+	afterid?: string;
+	unread?: number;
+}
+
 export async function posts(
 	ef: IExecuteFunctions,
 	itemIndex: number,
 ): Promise<IDataObject> {
-	const accountId = ef.getNodeParameter('accountId', itemIndex);
-	const mailboxId = ef.getNodeParameter('mailboxId', itemIndex);
+
+	const accountId = ef.getNodeParameter('accountId', itemIndex) as string;
+	const mailboxId = ef.getNodeParameter('mailboxId', itemIndex) as string;
+	const afterId = ef.getNodeParameter('afterId', itemIndex) as string;
+	const unread = ef.getNodeParameter('unread', itemIndex) as boolean;
+	const qs: QueryParams = {
+		mailboxid: mailboxId,
+	};
+	if (afterId) qs.afterid = afterId
+	if (unread) qs.unread = 1;
+
 	const options: IHttpRequestOptions = {
 		method: 'GET' as IHttpRequestMethods,
-		url: `/mail/account/${accountId}/posts`,
-		qs: {
-			mailboxid: mailboxId,
-		}
+		url: `/api/v1/mail/account/${accountId}/posts`,
+		qs
 	};
 
 	return await finkoperApiRequest<IDataObject>(ef, options);
--- nodes/FinkoperApi/properties/mail.fields.ts
+++ nodes/FinkoperApi/properties/mail.fields.ts
@@ -14,14 +14,13 @@ export const mailFields: INodeProperties[] = [
 			},
 		},
 	},
-
+	//posts
 	{
 		displayName: 'Mail mailboxId',
 		name: 'mailboxId',
 		type: 'string' as NodePropertyTypes,
 		default: '',
 		required: true,
-		description: 'List all mail from box',
 		displayOptions: {
 			show: {
 				resource: ['mail'],
@@ -29,5 +28,110 @@ export const mailFields: INodeProperties[] = [
 			},
 		},
 	},
+	{
+		displayName: 'Start Mail ID',
+		name: 'afterId',
+		type: 'string' as NodePropertyTypes,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['mail'],
+				operation: ['posts'],
+			},
+		},
+	},
+	{
+		displayName: 'Only Unread',
+		name: 'unread',
+		type: 'boolean' as NodePropertyTypes,
+		default: false,
+		displayOptions: {
+			show: {
+				resource: ['mail'],
+				operation: ['posts'],
+			},
+		},
+	},
+	//seen
+	{
+		displayName: 'BookkeeperTeamID',
+		name: 'bookkeeperTeamID',
+		type: 'string' as NodePropertyTypes,
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['mail'],
+				operation: ['seen'],
+			},
+		},
+	},
+	{
+		displayName: 'Seen',
+		name: 'seen',
+		type: 'boolean' as NodePropertyTypes,
+		default: true,
+		displayOptions: {
+			show: {
+				resource: ['mail'],
+				operation: ['seen'],
+			},
+		},
+	},
+	// {
+	// 	displayName: 'Items',
+	// 	name: 'items',
+	// 	type: 'fixedCollection',
+	// 	default: {},
+	// 	required: true,
+	// 	typeOptions: {
+	// 		multipleValues: true,
+	// 	},
+	// 	displayOptions: {
+	// 		show: {
+	// 			resource: ['mail'],
+	// 			operation: ['seen'],
+	// 		},
+	// 	},
+	// 	options: [
+	// 		{
+	// 			name: 'metadataValues',
+	// 			displayName: 'Metadata',
+	// 			values: [
+	// 				{
+	// 					displayName: 'From',
+	// 					name: 'from',
+	// 					type: 'string',
+	// 					default: '',
+	// 					required: true,
+	// 				},
+	// 				{
+	// 					displayName: 'Posts',
+	// 					name: 'posts',
+	// 					type: 'string',
+	// 					default: '',
+	// 					required: true,
+	// 					typeOptions: {
+	// 						multipleValues: true,
+	// 					},
+	// 					description: 'Posts IDs',
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// },
+	{
+		displayName: 'Items',
+		name: 'items',
+		type: 'json',
+		default: {},
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['mail'],
+				operation: ['seen'],
+			},
+		},
+	}
 
 ];
